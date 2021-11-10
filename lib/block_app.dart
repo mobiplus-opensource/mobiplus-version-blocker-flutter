eb42eca11a307532628dbbf6d133096be26fac08
Tiago Silva <tsilvasantos38@gmail.com>
2021-11-10 17:28:42 -0300

    bloquear-app: estrutura de código inicial

---
+++ lib/block_app.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+class BlockApp {
+  BlockApp._();
+
+  static BlockApp get instance => BlockApp._();
+
+  String get _buildApp => Platform.version.split("+").last.trim();
+
+  void block(String build) {
+    build.trim();
+    if (build.isEmpty) return;
+    if (build.compareTo(_buildApp) == 0) _blockAppAction();
+  }
+
+  void _blockAppAction() {}
+}
