baa39702787e97004736a5a1a55c548740881a15
Wellington Felisberto <wellington.silva@mobiplus.com.br>
2021-12-02 11:26:07 -0300

    structuring blocked screen

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -16,6 +16,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const BlockScreen();
+    return BlockScreen();
   }
 }
--- app/lib/screens/block.screen.dart
+++ app/lib/screens/block.screen.dart
@@ -1,25 +1,48 @@
 import 'package:flutter/material.dart';
 
 class BlockScreen extends StatefulWidget {
-  const BlockScreen({Key? key}) : super(key: key);
-  final String titleText = "Tá na hora de atualizar seu aplicativo";
-  final String middleText = "Fizemos algumas atualizações desde a ultima vez por aqui";
+  BlockScreen({Key? key}) : super(key: key);
+
+  @override
+  State<BlockScreen> createState() {
+    return BlockScreenStates();
+  }
+}
+
+class BlockScreenStates extends State<BlockScreen> {
+  String titleText = "Tá na hora de atualizar seu aplicativo";
+  final String middleText =
+      "Fizemos algumas atualizações desde a ultima vez por aqui";
   final String bottomText = "Clica aí no botão para baixar a nova versão";
   final String buttonText = "ATUALIZAR";
 
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const Scaffold(
+      home: Scaffold(
         body: Center(
-          child: Text('Hello World'),
+          child: Column(
+            children: [
+              Text(
+                titleText,
+                style: TextStyle(fontSize: 20),
+              ),
+              Text(
+                middleText,
+                style: TextStyle(fontSize: 20),
+              ),
+              Text(
+                bottomText,
+                style: TextStyle(fontSize: 20),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text(buttonText),
+              ),
+            ],
+          ),
         ),
       ),
     );
-  }
-
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
   }
 }
