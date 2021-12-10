2ac6616c8b2eae37a40b5e317989628c31c58cbd
Wellington Felisberto <wellington.silva@mobiplus.com.br>
2021-12-10 15:26:17 -0300

    catching values from realtime database

--- sample/lib/screens/home.screen.dart
+++ sample/lib/screens/home.screen.dart
@@ -1,9 +1,12 @@
 import 'package:example/screens/block.screen.dart';
+import 'package:example/screens/version_blocker/version.blocker.functions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
@@ -21,11 +24,12 @@ class HomeScreen extends StatelessWidget {
                         textStyle: MaterialStateProperty.all<TextStyle>(
                             TextStyle(fontSize: 20)))),
                 child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => BlockScreen()),
-                    );
+                  onPressed: () async {
+                    final blockVersion = await initVersionBlocker();
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(builder: (context) => BlockScreen()),
+                    // );
                   },
                   child: Text('Me clique'),
                 ),
