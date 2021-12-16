c1647103b57a4141196248e55c013647f19501a4
Wellington Felisberto <wellington.silva@mobiplus.com.br>
2021-12-16 10:28:26 -0300

    connection with realtime database in lib

--- lib/block_app.dart
+++ lib/block_app.dart
@@ -1,11 +1,15 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:version_blocker_flutter/exceptions/exceptions_imports.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
 class BlockApp {
-
   static late final PackageInfo _info;
+  final _child = FirebaseDatabase.instanceFor(app: Firebase.app()).ref().child('blockedVersions');
 
   Text? _message;
   static late final BuildContext _context;
@@ -18,7 +22,7 @@ class BlockApp {
   RegExp get _regexContainsOnlyNumbers => RegExp(r"^[0-9]+$");
 
   void initBlockApp() async {
-    await Firebase.initializeApp();
+    initVersionBlocker();
   }
 
   Text get _messageValue =>
@@ -52,4 +56,54 @@ class BlockApp {
   }
 
   void _blockAppAction() => showViewBlock();
+
+  Future<bool> initVersionBlocker() {
+    _child.onChildChanged.listen((_) => checkAndBlockVersion());
+    return checkAndBlockVersion();
+  }
+
+  Future<bool> checkAndBlockVersion() async {
+    final snapshot = await _child.once();
+    final value = snapshot.snapshot.value as dynamic;
+    final blockData = BlockData(
+      androidBuildNumber: value['androidBuildNumber'] as int,
+      iosBuildNumber: value['iosBuildNumber'] as int,
+    );
+
+    final PackageInfo _info;
+    _info = await PackageInfo.fromPlatform();
+    final appBuildNumber = int.parse(_info.buildNumber);
+
+    final int checkVersion = Platform.isIOS
+        ? blockData.iosBuildNumber
+        : blockData.androidBuildNumber;
+    if (appBuildNumber > checkVersion) {
+      return false;
+    }
+    return true;
+  }
+}
+
+class BlockData {
+  BlockData({
+    required this.androidBuildNumber,
+    required this.iosBuildNumber,
+  });
+
+  factory BlockData.fromJson(String str) => BlockData.fromMap(json.decode(str));
+
+  factory BlockData.fromMap(Map<String, dynamic> json) => BlockData(
+        androidBuildNumber: json["androidBuildsNumber"],
+        iosBuildNumber: json["iosBuildsNumber"],
+      );
+
+  final int androidBuildNumber;
+  final int iosBuildNumber;
+
+  String toJson() => json.encode(toMap());
+
+  Map<String, dynamic> toMap() => {
+        "androidBuildsNumber": androidBuildNumber,
+        "iosBuildsNumber": iosBuildNumber,
+      };
 }
--- sample/lib/main.dart
+++ sample/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:version_blocker_flutter/block_app.dart';
 import 'package:version_blocker_flutter_example/screens/home.screen.dart';
@@ -5,6 +6,7 @@ import 'package:version_blocker_flutter_example/screens/home.screen.dart';
 Future<void> main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   final blockApp = BlockApp();
   blockApp.initBlockApp();
   runApp(const MyApp());
