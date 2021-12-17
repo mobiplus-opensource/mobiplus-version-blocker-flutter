d3a0b35f50a8bf4c93af6a226f5bb6d9ba4d7dda
Wellington Felisberto <wellington.silva@mobiplus.com.br>
2021-12-17 15:24:21 -0300

    removing unecessary archives

--- lib/block_app.dart
+++ lib/block_app.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:firebase/firebase.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
